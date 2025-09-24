11407d549109211c0e3c22d425c987370a94c104
Hussain Durrani <hussaindurrani85@gmail.com>
2025-09-24 21:35:48 +0500

    second Commit

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,7 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
     cout << "I'm Hussain Durrani" << endl;
+
+    cout << "I'm learning Agentic AI" << endl;
     return 0;
 }
